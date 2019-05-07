82136ceb4d3cf2bf8cbe6f083cae80e7d3d53361
ahmed <ahmed_khozam@yahoo.com>
2019-05-07 12:28:31 +0200

    preparing the layout 1-layout for home screen 2-layout for the app it self 3-link between to Activites by intents

---
+++ app/src/main/java/com/ahmedkhozam/myapplication/StartScreenActivity.java
@@ -0,0 +1,28 @@
+package com.ahmedkhozam.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+        Button buttonStartGame=findViewById(R.id.button_start_game);
+        buttonStartGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startQuiz();
+            }
+        });
+    }
+
+    private void startQuiz() {
+        Intent intent=new Intent(StartScreenActivity.this,QuizActivity.class);
+        startActivity(intent);
+    }
+}
